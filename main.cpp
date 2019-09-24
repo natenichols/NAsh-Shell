87c2521644dc98d5406eeb8f74ef134819c2817b
Andre Kurait <andrekurait@gmail.com>
2019-09-23 22:02:05 -0500

    fixed strncmp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include "NAsh.h"
-#include <string>
+#include <string.h>
 
 int main (int argc, char **argv, char **envp) {
 
-    if(argc > 1 && (strncmp(argv[1], "-v", 3) == 0 || strncmp(argv[1], "--version", 10))) {
+    if(argc > 1 && (strncmp(argv[1], "-v", 3) == 0 || strncmp(argv[1], "--version", 10) == 0)) {
         std::cout << "v0.0.1: Nathan Nichols and Andre Kurait: NAsh" << std::endl;
         return 0;
     }
 
+    std::string cmd;
     while (true) {
         // Display bash prompt
         std::cout << "NAsh> ";
-        exit;
+        std::getline(std::cin, cmd, '\n');
+        break;
     }
     
     return 0;
