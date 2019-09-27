1cd67d4db47bb3f169d2cc4f61c97aa870032d89
natenichols <natenichols@cox.net>
2019-09-27 13:57:49 -0500

    ls printing gibberish

--- NAsh.h
+++ NAsh.h
@@ -10,6 +10,7 @@
 class NAsh {
     private:
         std::map<std::string, char*> environmentVars;
+        int BSIZE = 256;
     
     public:
         NAsh() {
@@ -18,6 +19,7 @@ class NAsh {
 
         void execute(char* cmd, char* args) {
             pid_t pid, new_pid;
+            int pipefd[2];
             int status;
 
             //get all environment Vars associated with PATH
@@ -29,33 +31,48 @@ class NAsh {
             char* path = strtok(cstr, ":");
 
             //buffer for copying the path
-            char pathWithCmd[256];
+            char pathWithCmd[BSIZE];
+            char buffer[BSIZE];
 
-            while(path != NULL) {
-                strcpy(pathWithCmd, path);
-                strcat(pathWithCmd, "/");
-                strcat(pathWithCmd, cmd);
-                char* args[] = {pathWithCmd, *args, NULL};
-
-                pid = fork();
-
-                if(pid == 0) {
-                    execvp(args[0], args);
-                }
-                else if(pid > 0) {
-
-                    if( (new_pid = wait(&status)) < 0){
-                        perror("wait");
-                        _exit(1);
-                    }
-                }
-                else {
-                    //std::cout << "Fails" << std::endl;
-                    //throw std::runtime_error("Invalid fork");
-                }
-
-                path = strtok(NULL, ":");
+            if(pipe(pipefd) == -1) {
+                std::cout << "Pipe failed to build" << std::endl;
+                _exit(-1);
             }
+
+            pid = fork();
+
+            if(pid == 0) {
+
+                while(path != NULL) {
+                    strcpy(pathWithCmd, path);
+                    strcat(pathWithCmd, "/");
+                    strcat(pathWithCmd, cmd);
+                    char* args[] = {pathWithCmd, *args, (char*)0};
+
+                    dup2 (pipefd[1], STDOUT_FILENO);
+                    close(pipefd[0]);
+                    close(pipefd[1]);
+                    int res = execvp(args[0], args);
+
+                    if(res > 0) return;
+
+                    path = strtok(NULL, ":");
+                }
+            }
+            else if(pid > 0) {
+                close(pipefd[1]);
+                while(read(pipefd[0], buffer, BSIZE) != 0);
+                    std::cout << buffer;
+                wait(NULL);
+            }
+            else {
+               // std::cout << "Fails" << std::endl;
+                throw std::runtime_error("Invalid fork");
+            }
+
+            std::cout << std::endl;
+            
+            delete cstr;
         }
 
         void appendEnv(char* envVar, char* envVal) {
